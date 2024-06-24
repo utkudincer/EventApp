6d0d92df29df8c42b695ecbf60d49f1e9b0b65f3
Eren Utku Dincer <utkudincer@hotmail.com>
2024-06-24 12:02:13 +0300

    Fatihe selamlar

--- EventApp/ContentView.swift
+++ EventApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Eray!")
+            Text("Hello, Fatih!")
         }
         .padding()
     }
--- EventApp/DiscoverView.swift
+++ EventApp/DiscoverView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct DiscoverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+        Text("Eray")
+        }
+        .navigationTitle("Keşfet")
     }
 }
 
