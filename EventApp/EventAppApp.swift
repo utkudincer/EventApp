ff5094ba3e5c62556340948f3518f392cd37fef5
Eren Utku Dincer <utkudincer@hotmail.com>
2024-06-24 11:36:29 +0300

    Initial Commit

---
+++ EventApp/EventAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventAppApp.swift
+//  EventApp
+//
+//  Created by Eren Utku Dinçer on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
