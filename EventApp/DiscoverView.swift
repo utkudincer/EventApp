1fdbfcd8afe98153bfb60d6e8d596d72479aba6f
Eren Utku Dincer <utkudincer@hotmail.com>
2024-06-27 13:10:24 +0300

    Köyden Şehre Summit

--- EventApp/DiscoverView.swift
+++ EventApp/DiscoverView.swift
@@ -3,8 +3,7 @@ import SwiftUI
 let images = ["SliderImage1", "image2"]
 
 struct DiscoverView: View {
-    @State private var titleOffset: CGFloat = -250 // Başlık konumunu ayarlamak için
-    
+    @State private var titleOffset: CGFloat = -250 
 
     var body: some View {
         NavigationView {
@@ -22,7 +21,7 @@ struct DiscoverView: View {
                                     GroupBox(label:
                                         Text("IEEE Türkiye Xtreme Kampı' 24")
                                     ) {
-                                        HStack{
+                                        HStack {
                                             Text("22-26 Temmuz 2024")
                                                 .font(.caption)
                                         }
@@ -36,68 +35,63 @@ struct DiscoverView: View {
                             }
                             .padding(5)
                         }
-                        
                     }
                     .padding(3)
                 }
-                
-                .offset(CGSize(width: 3, height:-85))
-                // Custom title with offset,
+                .offset(CGSize(width: 3, height: -85))
+                // Custom title with offset
                 Text("Keşfet")
                     .font(.largeTitle)
                     .bold()
-                    .offset(x: -120 ,y:-320)
-                
-                
+                    .offset(x: -120, y: -320)
             }
-            .navigationBarHidden(true) // Hide the default navigation bar title
+            .toolbar {
+                ToolbarItemGroup() {
+                    QRSearchbar()
+                }
+            }
         }
-        
     }
 }
 
 struct QRSearchbar: View {
-        @State private var showSearchBar = false
-        @State private var showNotifications = false
-        @State private var showQRScanner = false
+    @State private var showSearchBar = false
+    @State private var showNotifications = false
+    @State private var showQRScanner = false
+
     var body: some View {
-        NavigationView{
-            VStack{
-                HStack(spacing:20){
-                    Button(action: {
-                                            showSearchBar.toggle()
-                                        }) {
-                                            Image(systemName: "magnifyingglass")
-                                        }
-                                        .sheet(isPresented: $showSearchBar) {
-                                            SearchView()
-                                        }
-                                        
-                                        Button(action: {
-                                            showNotifications.toggle()
-                                        }) {
-                                            Image(systemName: "bell")
-                                        }
-                                        .sheet(isPresented: $showNotifications) {
-                                            NotificationsView()
-                                        }
-                                        Button(action: {
-                                                showQRScanner.toggle()
-                                            }) {
-                                                Image(systemName: "qrcode")
-                                            }
-                                            .sheet(isPresented: $showQRScanner) {
-                                                QRScannerView()
-                                            }
-                }
+        HStack(spacing: 20) {
+            Button(action: {
+                showSearchBar.toggle()
+            }) {
+                Image(systemName: "magnifyingglass")
+            }
+            .sheet(isPresented: $showSearchBar) {
+                SearchView()
+            }
+
+            Button(action: {
+                showNotifications.toggle()
+            }) {
+                Image(systemName: "bell")
+            }
+            .sheet(isPresented: $showNotifications) {
+                NotificationsView()
+            }
+
+            Button(action: {
+                showQRScanner.toggle()
+            }) {
+                Image(systemName: "qrcode")
+            }
+            .sheet(isPresented: $showQRScanner) {
+                QRScannerView()
             }
         }
-        .padding(.top, 8.0)
-        .shadow(radius: 5)
-        
-            
     }
 }
+
+
 #Preview {
     DiscoverView()
 }
