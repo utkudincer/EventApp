b9223e2d19ed87845e2164969cba6cb126c6084a
Eren Utku Dincer <utkudincer@hotmail.com>
2024-07-30 18:54:09 +0300

    splashview yapıldı. ho karşim

---
+++ EventApp/SplashScreen.swift
@@ -0,0 +1,8 @@
+//
+//  SplashScreen.swift
+//  EventApp
+//
+//  Created by Eren Utku Dinçer on 30.07.2024.
+//
+
+import Foundation
---
+++ EventApp/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  EventApp
+//
+//  Created by Eren Utku Dinçer on 30.07.2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
